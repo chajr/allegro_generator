034b1cf4644140b677fde2cefee43635cc56818a
chajr <git@bluetree.pl>
2014-09-06 17:47:12 +0200

    login page (issue #1)
    This version introduces multiple enhancements:
    
    show add form depend of logged in user
    added button to clear form
    removed header and footer from error message
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      BLUE_FRAMEWORK/modules/allegro_generator/layouts/user_layouts/1/add_form.html
                -- Added --
                    test user add form
    
    Modified:   BLUE_FRAMEWORK/modules/login/layouts/form.html
                -- Updated --
                    style of login button
    
    Modified:   BLUE_FRAMEWORK/modules/allegro_generator/allegro_generator.php
                -- Added --
                    _showUserAddForm
                    _getUserId
                    TEMPLATE_DIR to store path for user templates
                -- Updated --
                    run lunch _showUserAddForm when user is logged in
    
    Modified:   BLUE_FRAMEWORK/elements/css/base.css
                -- Added --
                    form and navigation styles
    
    Modified:   BLUE_FRAMEWORK/modules/allegro_generator/layouts/index.html
                -- Added --
                    clear form button and marker for user templates
    
    Modified:   BLUE_FRAMEWORK/elements/layouts/error_critic.html
                -- Removed --
                    header and footer of message block

--- BLUE_FRAMEWORK/modules/allegro_generator/allegro_generator.php
+++ BLUE_FRAMEWORK/modules/allegro_generator/allegro_generator.php
@@ -7,12 +7,14 @@
  * @subpackage  generator
  * @author      Michał Adamiak    <chajr@bluetree.pl>
  * @copyright   chajr/bluetree
- * @version     0.2.0
+ * @version     0.3.0
  */
 class allegro_generator extends module_class
 {
     public $requireLibraries    = [];
     public $requireModules      = [];
+
+    const TEMPLATE_DIR = 'modules/allegro_generator/layouts/user_layouts/';
 
     /**
      * information that use is logged in
@@ -30,8 +32,39 @@ class allegro_generator extends module_class
 
         if ($this->_verification) {
             $this->layout('index');
+            $this->_showUserAddForm();
         }
-        
+    }
+
+    /**
+     * show add form template for current user
+     */
+    protected function _showUserAddForm()
+    {
+        $userId      = $this->_getUserId();
+        $path        = self::TEMPLATE_DIR . $userId . '/add_form.html';
+        $independent = new display_class([
+            'independent' => true,
+            'template'    => $path
+        ]);
+
+        $this->generate('user_template', $independent->render());
+    }
+
+    /**
+     * get user id from session
+     * 
+     * @return null|int
+     */
+    protected function _getUserId()
+    {
+        $userData = $this->session->returns('user');
+
+        if (isset($userData['log_class_uid'])) {
+            return $userData['log_class_uid'];
+        }
+
+        return null;
     }
 
     public function runErrorMode(){}
