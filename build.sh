d9a214c2f3b319bb497f24be4b3a0c060e5f83b2
chajr <chajr@bluetree.pl>
2015-10-28 19:39:56 +0100

    added install script and loading bower packages

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+ENV=$1
+
+echo "Begin Allegro Generator installation for environment: ";
+echo "$ENV";
+
+npm install;
+
+if [ "$ENV" == "prod" ]; then
+    composer install --no-dev --optimize-autoloader --prefer-dist;
+else
+    composer diagnose;
+    composer install;
+fi
+
+echo "Installation complete.";
