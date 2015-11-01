36a95660b43ebebe166f5571e425d65e5e6e8ab3
chajr <chajr@bluetree.pl>
2015-11-01 18:03:52 +0100

    added styles and js scripts to main page and added cache configuration

--- app/Mod/MainPage.php
+++ app/Mod/MainPage.php
@@ -8,11 +8,15 @@ class MainPage extends Base
 {
     public function execute(Slim $app)
     {
-        $loader = new \Twig_Loader_Filesystem('../app/templates');
-        $twig = new \Twig_Environment($loader, array(
-            'cache' => '../var/cache',
-        ));
-        $template = $twig->loadTemplate('main_container.html');
+        $config = [];
+
+        if ($app->config('app')['cache']['enabled']) {
+            $config['cache'] = $app->config('app')['cache']['dir'];
+        }
+
+        $loader     = new \Twig_Loader_Filesystem('../app/templates');
+        $twig       = new \Twig_Environment($loader, $config);
+        $template   = $twig->loadTemplate('main_container.html');
 
         return $template->render(['some_var' => 'template']);
     }
